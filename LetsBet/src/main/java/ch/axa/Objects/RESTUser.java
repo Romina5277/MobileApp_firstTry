b7d107fc6a53da996b3e312467a4b2d47e1afd74
Romina Ferrario <saemyferrario@outlook.com>
2019-03-19 13:32:44 +0100

    REST-Objekte erstellt

---
+++ LetsBet/src/main/java/ch/axa/Objects/RESTUser.java
@@ -0,0 +1,71 @@
+package ch.axa.Objects;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class RESTUser {
+    private int id;
+    private String username;
+    private String firstname;
+    private String lastname;
+    private Date birthday;
+    private String mail;
+
+    public RESTUser(int id, String username, String firstname, String lastname, Date birthday, String mail) {
+        this.id = id;
+        this.username = username;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.birthday = birthday;
+        this.mail = mail;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
--- LetsBet/src/main/java/ch/axa/Service/Service.java
+++ LetsBet/src/main/java/ch/axa/Service/Service.java
@@ -3,12 +3,15 @@ package ch.axa.Service;
 
 import ch.axa.DAO.DAO;
 import ch.axa.Objects.Bet;
+import ch.axa.Objects.RESTBet;
+import ch.axa.Objects.RESTUser;
 import ch.axa.Objects.User;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,11 +34,51 @@ public class Service {
      * User
      */
 
+    public User restUserToUser(RESTUser restUser) {
+        User user = new User();
+        Optional<User> originalUser = dao.getUser(restUser.getId());
+
+        user.setId(restUser.getId());
+        user.setUsername(restUser.getUsername());
+        user.setFirstname(restUser.getFirstname());
+        user.setLastname(restUser.getLastname());
+        user.setBirthday(restUser.getBirthday());
+        user.setMail(restUser.getMail());
+
+        if (originalUser.isPresent()) {
+            user.setFriends(originalUser.get().getFriends());
+            user.setInvolvements(originalUser.get().getInvolvements());
+            user.setBets(originalUser.get().getBets());
+        } else {
+            user.setFriends(new ArrayList<>());
+            user.setInvolvements(new ArrayList<>());
+            user.setBets(new ArrayList<>());
+        }
+
+        return user;
+    }
+
+    public RESTUser userToRESTUser(Optional<User> user) {
+        if (user.isPresent()) {
+            return new RESTUser(user.get().getId(), user.get().getUsername(), user.get().getFirstname(),
+                                    user.get().getLastname(), user.get().getBirthday(), user.get().getMail());
+        } else {
+            return null;
+        }
+    }
+
     @GET
     @Path("/users")
     @Produces({"application/json"})
-    public List<User> getUsers() {
-        return dao.getUsers();
+    public List<RESTUser> getUsers() {
+        List<User> userList = dao.getUsers();
+        List<RESTUser> restUserList = new ArrayList<>();
+
+        for (User user : userList) {
+            restUserList.add(userToRESTUser(Optional.ofNullable(user)));
+        }
+
+        return restUserList;
     }
 
     @GET
@@ -46,7 +89,7 @@ public class Service {
 
         if(user.isPresent()) {
             return Response.status(200)
-                    .entity(user.get())
+                    .entity(userToRESTUser(user))
                     .build();
         } else {
             return Response.status(404)
@@ -58,11 +101,52 @@ public class Service {
      * Bet
      */
 
+    public Bet restBetToBet(RESTBet restBet) {
+        Bet bet = new Bet();
+        Optional<Bet> originalBet = dao.getBet(restBet.getId());
+
+        bet.setId(restBet.getId());
+        bet.setTitle(restBet.getTitle());
+        bet.setTypist(restUserToUser(restBet.getTypist()));
+        bet.setEnd(restBet.getEnd());
+        bet.setInput(restBet.getInput());
+        bet.setDetail(restBet.getDetail());
+        bet.setLastchange(restBet.getLastchange());
+        bet.setPlace(restBet.getPlace());
+
+        if (originalBet.isPresent()) {
+            bet.setInvolvements(originalBet.get().getInvolvements());
+        } else {
+            bet.setInvolvements(new ArrayList<>());
+        }
+
+        return bet;
+    }
+
+    public RESTBet betToRESTBet(Optional<Bet> bet) {
+        if (bet.isPresent()) {
+            return new RESTBet(bet.get().getId(), bet.get().getTitle(), userToRESTUser(Optional.ofNullable(bet.get().getTypist())),
+                                bet.get().getEnd(),  bet.get().getInput(), bet.get().getDetail(), bet.get().getLastchange(),
+                                bet.get().getPlace());
+        } else {
+            return null;
+        }
+
+
+    }
+
     @GET
     @Path("/bets")
     @Produces({"application/json"})
-    public List<Bet> getBets() {
-        return dao.getBets();
+    public List<RESTBet> getBets() {
+        List<Bet> betList = dao.getBets();
+        List<RESTBet> restBetList = new ArrayList<>();
+
+        for (Bet bet : betList) {
+            restBetList.add(betToRESTBet(Optional.ofNullable(bet)));
+        }
+
+        return restBetList;
     }
 
     @GET
@@ -73,7 +157,7 @@ public class Service {
 
         if(bet.isPresent()) {
             return Response.status(200)
-                    .entity(bet.get())
+                    .entity(betToRESTBet(Optional.of(bet.get())))
                     .build();
         } else {
             return Response.status(404)
@@ -84,12 +168,12 @@ public class Service {
     @PUT
     @Path("/bet/{id}")
     @Produces({"application/json"})
-    public Response changeBet(@PathParam("id") int id, Bet bet) {
-        Optional<Bet> changedBet = dao.changeBet(id, bet);
+    public Response changeBet(@PathParam("id") int id, RESTBet restBet) {
+        Optional<Bet> changedBet = dao.changeBet(id, restBetToBet(restBet));
 
         if(changedBet.isPresent()){
             return Response.status(201)
-                    .entity(changedBet.get())
+                    .entity(betToRESTBet(Optional.of(changedBet.get())))
                     .build();
         } else {
             return Response.status(404)
@@ -100,12 +184,12 @@ public class Service {
     @POST
     @Path("/bet")
     @Produces({"application/json"})
-    public Response createBet(Bet bet) {
-        Optional<Bet> addedBet = dao.createPerson(bet);
+    public Response createBet(RESTBet restBet) {
+        Optional<Bet> addedBet = dao.createPerson(restBetToBet(restBet));
 
         if(addedBet.isPresent()){
             return Response.status(201)
-                    .entity(addedBet.get())
+                    .entity(betToRESTBet(Optional.of(addedBet.get())))
                     .build();
         } else {
             return Response.status(404)
@@ -118,10 +202,17 @@ public class Service {
     @Produces({"application/json"})
     public Response deleteBet(@PathParam("id") int id) {
         Optional<Bet> deletedBet = dao.deletePerson(id);
+        List<Bet> betList = dao.getBets();
+        List<RESTBet> restBetList = new ArrayList<>();
+
+        for (Bet bet : betList) {
+            restBetList.add(betToRESTBet(Optional.ofNullable(bet)));
+        }
+
 
         if(deletedBet.isPresent()){
             return Response.status(201)
-                    .entity(dao.getBets())
+                    .entity(restBetList)
                     .build();
         } else {
             return Response.status(404)
